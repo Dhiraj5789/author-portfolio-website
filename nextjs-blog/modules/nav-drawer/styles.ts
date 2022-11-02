91497b019b899100f312995139b3aba1949df07b
Dhiraj5789 <dhirajsuthar5789@gmail.com>
2022-11-02 15:56:18 +0530

    landing page structure

---
+++ nextjs-blog/modules/nav-drawer/styles.ts
@@ -0,0 +1,7 @@
+import { css } from "@emotion/css";
+
+export const muiPaper = css`
+opacity: 0.9;
+background-color: rgba(0,96,206,.95);
+z-index:2;
+width:50vw;`
---
+++ nextjs-blog/pages/_app.tsx
@@ -0,0 +1,6 @@
+import Home from ".";
+import "./global.css";
+
+export const App = () => <Home />
+
+export default App;

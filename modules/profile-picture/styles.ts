bff8246163017c25f556cc2ca3595e6cd3de19b1
Dhiraj Suthar <dhirajsuthar5789@gmail.com>
2022-11-12 20:06:43 +0530

    dynamic aside component

--- modules/profile-picture/styles.ts
+++ modules/profile-picture/styles.ts
@@ -1,13 +1,18 @@
 import styled from "@emotion/styled";
 import { PRIMARY_BLUE } from "../colors";
 
-export const ProfilePictureContainer = styled.aside`
+type BackgroumdProps = {
+  backgroundType: "contact" | "default";
+};
+
+export const ProfilePictureContainer = styled.aside<BackgroumdProps>`
   position: fixed;
   left: 0;
   top: 0;
   width: 50%;
   height: 100vh;
-  background-color: ${PRIMARY_BLUE};
+  background: ${(props) => (props.backgroundType ? "hotpink" : "turquoise")};
+  background: ${PRIMARY_BLUE};
   @media (max-width: 655px) {
     display: none;
   }
